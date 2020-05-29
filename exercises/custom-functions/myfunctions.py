2caf0f87506129b2887441b6a78ba870d57d311c
Rishi Malhotra <rishirmalhotra@gmail.com>
2020-05-28 20:16:23 -0400

    create myfunctions.py
    create myfunctions.py

---
+++ exercises/custom-functions/myfunctions.py
@@ -0,0 +1,22 @@
+# custom-functions/my_functions.py
+
+# TODO: define temperature conversion function here
+
+# TODO: define gradebook function here
+
+if __name__ == "__main__":
+
+    print("--------------------")
+    print("CUSTOM FUNCTIONS EXERCISE...")
+
+    #print("--------------------")
+    #c = 0
+    #print("THE CELSIUS TEMP IS:", c, "DEGREES")
+    #f = celsius_to_fahrenheit(c)
+    #print("THE FAHRENHEIT EQUIVALENT IS:", f, "DEGREES")
+
+    #print("--------------------")
+    #score = 87.5
+    #print("THE NUMERIC SCORE IS:", score)
+    #grade = numeric_to_letter_grade(score)
+    #print("THE LETTER-GRADE EQUIVALENT IS:", grade)
