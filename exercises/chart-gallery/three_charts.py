ec1978e354cbf620e04543b740fea4dab5342d20
Rishi Malhotra <rishirmalhotra@gmail.com>
2020-06-04 19:36:56 -0400

    Update three_charts.py

--- exercises/chart-gallery/three_charts.py
+++ exercises/chart-gallery/three_charts.py
@@ -1,2 +1,14 @@
 three_charts.py
 
+# adapted from: https://plot.ly/python/pie-charts/
+#https://github.com/prof-rossetti/intro-to-python/blob/master/notes/python/packages/plotly.md
+
+import plotly
+import plotly.graph_objs as go
+
+labels = ["Oxygen", "Hydrogen", "Carbon_Dioxide", "Nitrogen"]
+values = [4500, 2500, 1053, 500]
+
+trace = go.Pie(labels=labels, values=values)
+
+plotly.offline.plot([trace], filename="basic_pie_chart.html", auto_open=True)
